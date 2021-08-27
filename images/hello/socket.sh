a7bd6264a586fef63ff6e461f166a823eb9ad73e
ttgkowalski <kowalskittg@gmail.com>
2021-08-27 19:44:50 -0300

    Automation scripts

--- images/hello/socket.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm ../../sockets/firecracker.socket
-
-../../bin/firecracker --api-sock ../../sockets/firecracker.socket
--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # KERNEL
-curl --unix-socket ../../sockets/firecracker.socket -i \
+curl --unix-socket sockets/$1.socket -i \
       -X PUT 'http://localhost/boot-source'   \
       -H 'Accept: application/json'           \
       -H 'Content-Type: application/json'     \
       -d "{
-            \"kernel_image_path\": \"$(pwd)/vmlinux.bin\",
+            \"kernel_image_path\": \"$(pwd)/images/hello/vmlinux.bin\",
             \"boot_args\": \"console=ttyS0 reboot=k panic=1 pci=off\"
        }"
 
 # ROOTFS
-curl --unix-socket ../../sockets/firecracker.socket -i \
+curl --unix-socket sockets/$1.socket -i \
     -X PUT 'http://localhost/drives/rootfs' \
     -H 'Accept: application/json'           \
     -H 'Content-Type: application/json'     \
     -d "{
         \"drive_id\": \"rootfs\",
-        \"path_on_host\": \"$(pwd)/rootfs.ext4\",
+        \"path_on_host\": \"$(pwd)/images/hello/rootfs.ext4\",
         \"is_root_device\": true,
         \"is_read_only\": false
     }"
@@ -25,7 +25,7 @@ curl --unix-socket ../../sockets/firecracker.socket -i \
 
 
 # NETWORK
-curl --unix-socket ../../sockets/firecracker.socket -i \
+curl --unix-socket sockets/$1.socket -i \
   -X PUT 'http://localhost/network-interfaces/eth0' \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
@@ -36,7 +36,7 @@ curl --unix-socket ../../sockets/firecracker.socket -i \
     }'
 
 # START
-curl --unix-socket ../../sockets/firecracker.socket -i \
+curl --unix-socket sockets/$1.socket -i \
     -X PUT 'http://localhost/actions'       \
     -H  'Accept: application/json'          \
     -H  'Content-Type: application/json'    \
